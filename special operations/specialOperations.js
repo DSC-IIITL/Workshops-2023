73947010d462992b3feb2b731f0a1f526b8cb0f0
harshitagupta06 <lcs2022022@iiitl.ac.in>
2023-12-15 20:41:34 +0530

    addtional topics

---
+++ special operations/specialOperations.js
@@ -0,0 +1,40 @@
+//Ternary Operator
+const age = 25;
+
+// Using the ternary operator to assign a value based on a condition
+const message = age >= 18 ? "You are an adult" : "You are a minor";
+
+console.log(message);  // Outputs: You are an adult
+
+
+//nullish coalescing operator ??
+
+// Without nullish coalescing operator
+//let variable = null;
+
+//let result = (variable !== null && variable !== undefined) ? variable : "default value";
+//console.log(result);  // Outputs: default value
+
+//With the nullish coalescing operator:
+// With nullish coalescing operator
+//let variable = null;
+
+//let result = variable ?? "default value";
+//console.log(result);  // Outputs: default value
+
+
+//difference between the nullish coalescing operator and the logical OR operator (||):
+const defaultValue = "default value";
+let variable1 = null;
+let variable2 = "";
+
+let result1 = variable1 ?? defaultValue;
+let result2 = variable2 || defaultValue;
+
+console.log(result1);  // Outputs: default value
+console.log(result2);  // Outputs: default value
+
+
+
+
+
